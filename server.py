a2120ccebf1237cab431aec9f63313f5867d151d
孙雨生 <11139191@sunyusheng.local>
2022-09-29 11:04:57 +0800

    fmt

--- server.py
+++ server.py
@@ -2,5 +2,10 @@ import http.server as BaseHTTPServer
 import ssl
 
 httpd = BaseHTTPServer.HTTPServer(('127.0.0.1', 8080), BaseHTTPServer.SimpleHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./127.0.0.1.pem', keyfile='./127.0.0.1-key.pem', server_side=True, ssl_version=ssl.PROTOCOL_TLSv1_2)
+httpd.socket = ssl.wrap_socket(
+    httpd.socket,
+    certfile='./127.0.0.1.pem',
+    keyfile='./127.0.0.1-key.pem',
+    server_side=True,
+    ssl_version=ssl.PROTOCOL_TLSv1_2)
 httpd.serve_forever()
